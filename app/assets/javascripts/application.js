75e27e2d6ac26ebd7be43500f80290bedd34111b
Dominic Michaud <dominic@ateliermade.ca>
2017-01-10 09:52:33 -0500

    Add index action / bootstrap template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require bootstrap-sprockets
+//= require jqBootstrapValidation
+//= require clean-blog.js
+// require_tree .
