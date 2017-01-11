b202f6e5e13bb7f0cf5008f4d894b02360f66e12
Dominic Michaud <dominic@ateliermade.ca>
2017-01-11 11:20:05 -0500

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110163816) do
+ActiveRecord::Schema.define(version: 20170111143019) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20170110163816) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "subtitle"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
