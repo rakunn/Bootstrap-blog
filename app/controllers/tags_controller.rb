1ce98a52322648f1966903cb2c91c1f51593d912
Dominic Michaud <dominic@ateliermade.ca>
2017-01-12 15:58:12 -0500

    Added Tags index, edit, delete actions / template

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,36 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update_attributes(tag_params)
+      redirect_to tags_path, :notice => 'Tag updated with success!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.taggings.destroy_all
+    if @tag.destroy
+      redirect_to tags_path, :notice => 'Tag deleted with success!'
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
